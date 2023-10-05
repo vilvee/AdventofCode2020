f29148a191357f40077170352253062c4c965433
Veronika <116470467+vilvee@users.noreply.github.com>
2023-10-05 10:15:31 -0400

    done

---
+++ Day 2/Day 2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Day_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "./data.txt";
+            string output, line;
+            List<int> nums = new List<int>();
+            int result;
+
+            try
+            {
+                // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader(input))
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        nums.Add(int.Parse(line));
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                for (int j = 0; j < nums.Count; j++)
+                {
+                    for(int k = 0; k < nums.Count; k++)
+                    {
+                        if (nums[i] + nums[j] + nums[k] == 2020)
+                        {
+                            Console.WriteLine(nums[i] * nums[j] * nums[k]);
+                            break;
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+    }
+
