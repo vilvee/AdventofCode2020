ea23c8062fd4d40a814154442b146cf311e987b7
Veronika <116470467+vilvee@users.noreply.github.com>
2023-10-07 19:17:39 -0400

    part 2

--- Day 3/Day 3/Program.cs
+++ Day 3/Day 3/Program.cs
@@ -13,11 +13,30 @@ namespace Day_2
 
             char[][] pattern = patternLines.Select(x => x.ToArray()).ToArray();
 
-            List<char> extracted = ExtractPatternCharacters(pattern, 3, 1);
+            //part 1
+           // List<char> extracted = ExtractPatternCharacters(pattern, 3, 1);
+           // int treesCount = extracted.Count(c => c == '#');
+           // Console.WriteLine(treesCount);
 
-            int treesCount = extracted.Count(c => c == '#');
-            Console.WriteLine(treesCount);
+            //part 2
+            int[] rightSlopes = { 1, 3, 5, 7, 1 };
+            int[] downSlopes = { 1, 1, 1, 1, 2 };
+            List<char> extracted = new List<char>();
+            int[] numsOfTrees = new int[rightSlopes.Length];
 
+            for (int i = 0; i < rightSlopes.Length; i++)
+            {
+                extracted = ExtractPatternCharacters(pattern, rightSlopes[i], downSlopes[i]);
+                numsOfTrees[i] = extracted.Count(c => c == '#');
+            }
+
+            foreach (var tree in numsOfTrees)
+            {
+                Console.WriteLine(tree);
+            }
+
+            long prod = numsOfTrees.Aggregate(1L, (a, b) => a * b);
+            Console.WriteLine(prod);
         }
 
         public static List<char> ExtractPatternCharacters(char[][] input, int right, int down)
@@ -29,7 +48,7 @@ namespace Day_2
 
             for (int x = 0, y = 0; y < height; x += right, y += down)
             {
-                extractedChars.Add(input[y][x % width]);
+                extractedChars.Add(input[y][x % width]); //wrap around
             }
 
             return extractedChars;
--- Day 3/Day 3/obj/Debug/net8.0/Day 3.AssemblyInfo.cs
+++ Day 3/Day 3/obj/Debug/net8.0/Day 3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day 3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce5dbd294199447742f22383179c640ba02b95be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c97ff6ceb3fcc5126c5c96fd3ca32f52e4bd2756")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day 3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day 3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
